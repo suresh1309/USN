639cb349b3280756ed0d1335c055949eabf8184d
suresh1309 <89911093+suresh1309@users.noreply.github.com>
2023-07-11 15:30:09 +0530

    Update main.tf

--- src/terraform/verkor-azure-infra-hub/main.tf
+++ src/terraform/verkor-azure-infra-hub/main.tf
@@ -292,7 +292,7 @@ module "keyvault" {
   purge_protection_enabled        = var.purge_protection_enabled
   tags                            = merge({ "ResourceName" = format("%s", local.resource_names.keyvault_name) }, local.tags)
 
-  
+  depends_on          = [module.azurerm_rg_transit]
 }
 # This resources helps in creating Random Password for Key Secret for Jumphost VM1 
 resource "random_password" "jumpvm1" {
